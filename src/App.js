0ecfd125d465df243ed856d2be2484fa4cfd2444
Lisa Koss <lisakoss@uw.edu>
2016-11-29 21:15:46 -0800

    Add homepage styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Layout, Header, Navigation} from 'react-mdl';
+import {Layout, Header, Navigation, Textfield} from 'react-mdl';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Layout style={{background: 'url(./img/IMG_7962.png) center / cover'}}>
+        <Layout fixedHeader style={{background: 'url(./img/IMG_7962.png) center / cover'}}>
           <Header transparent title="BANDIT" style={{color: 'white'}}>
             <Navigation>
               <a href="">Board</a>
@@ -14,6 +14,17 @@ class App extends React.Component {
               <a href="">Profile</a>
             </Navigation>
           </Header>
+          <div className="welcome-msg">
+            <p className="welcome-title">band together</p>
+            <p>find your next gig, band, or collaboration</p>
+            <p>breakthrough locally or worldwide</p>
+            <i className="fa fa-search" aria-hidden="true"></i>
+<Textfield
+    onChange={() => {}}
+    label="search"
+    style={{width: '200px'}}
+/>
+          </div>
         </Layout>
       </div>
     );
