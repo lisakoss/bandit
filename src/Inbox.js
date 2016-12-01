d69596b376d03f71ef695a595c936a724a73689b
Peachie <ultimate.peachie@yahoo.com>
2016-12-01 15:38:21 -0800

    Start of the design

--- src/Inbox.js
+++ src/Inbox.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Button, List, ListItem, ListItemContent, ListItemAction, Icon } from 'react-mdl';
 
 class Inbox extends React.Component {
     render() {
         return (
-            <div className="content-container"><h1>inbox</h1></div>
+            <div>
+                <div className="content-container"><h1>inbox</h1><Button raised colored>Compose</Button></div>
+                <div>
+                    <List style={{ width: '30%' }}>
+                        <ListItem threeLine>
+                            <ListItemContent avatar="person" subtitle="Bryan Cranston played the role of Walter in Breaking Bad. He is also known for playing Hal in Malcom in the Middle.">Bryan Cranston</ListItemContent>
+                            <ListItemAction>
+                                <a href="#"><Icon name="star" /></a>
+                            </ListItemAction>
+                        </ListItem>
+                        <ListItem threeLine>
+                            <ListItemContent avatar="person" subtitle="Aaron Paul played the role of Jesse in Breaking Bad. He also featured in the Need For Speed Movie.">Aaron Paul</ListItemContent>
+                            <ListItemAction>
+                                <a href="#"><Icon name="star" /></a>
+                            </ListItemAction>
+                        </ListItem>
+                        <ListItem threeLine>
+                            <ListItemContent avatar="person" subtitle="Bob Odinkrik played the role of Saul in Breaking Bad. Due to public fondness for the character, Bob stars in his own show now, called Better Call Saul.">Bob Odenkirk</ListItemContent>
+                            <ListItemAction>
+                                <a href="#"><Icon name="star" /></a>
+                            </ListItemAction>
+                        </ListItem>
+                    </List>
+                </div>
+            </div>
         );
     }
 }
