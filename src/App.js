32fe199cab80ac087879ecfb92f788df4611560b
Alexis Koss <alexis30@uw.edu>
2016-11-30 20:35:19 -0800

    Add user profile image in navigation bar when logged in

--- src/App.js
+++ src/App.js
@@ -2,9 +2,45 @@ import React from 'react';
 import './index.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import SignOut from './SignOut';
+import firebase from 'firebase';
 
 class App extends React.Component {
+	constructor(props){
+			super(props);
+			this.state = {}
+		}
+	//Lifecycle callback executed when the component appears on the screen.
+  //It is cleaner to use this than the constructor for fetching data
+  componentDidMount() {
+    /* Add a listener and callback for authentication events */
+    this.unregister = firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.setState({userId:user.uid});
+				this.setState({displayName:firebase.auth().currentUser.displayName});
+				this.setState({avatar:firebase.auth().currentUser.photoURL});
+				console.log(this.state.avatar);
+      }
+      else{
+        this.setState({userId: null}); //null out the saved state
+				this.setState({displayName: null}); //null out the saved state
+				this.setState({avatar: null}); //null out the saved state
+      }
+    })
+  }
+
+  //when the component is unmounted, unregister using the saved function
+  componentWillUnmount() {
+    if(this.unregister){ //if have a function to unregister with
+      this.unregister(); //call that function!
+    }
+  }
+
 	render() {
+		var profileImg = null;
+
+		if(this.state.userId !== null) {
+			profileImg = <img src={this.state.avatar} alt="avatar" className="profile-img" />;
+		}
     return (
       <div style={{height: '100%'}}>
         <Layout fixedHeader>
@@ -13,7 +49,7 @@ class App extends React.Component {
 							<a href="/#/board">Board</a>
 							<a href="/#/search">Search</a>
 							<a href="/#/inbox">Inbox</a>
-							<a href="/#/profile">Profile</a>
+							<a href={this.state.userId !== null ? "/#/profile" : "/#/login"}>{this.state.userId !== null ? this.state.displayName : 'Login'} {profileImg}</a> 
 						</Navigation>
           </Header>
           <Drawer title="Title">
