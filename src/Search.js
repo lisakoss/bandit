3580eba0fcfa515408f6d1e1b939ab30d2951ca2
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2016-12-05 10:12:30 -0800

    Add initial structure for search

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import firebase from 'firebase';
+import { Textfield, Button } from 'react-mdl';
 
 class Search extends React.Component {
-    render() {
-        return (
-            <div className="content-container"><h1>search</h1></div>
-        );
-    }
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+      return (
+        <div className="content-container">
+          <h1>search</h1>
+          <form role="form" className="search-form">
+            <Textfield
+              onChange={this.handleSearch}
+              label="Search for a musician, gig or collaboration"
+              id="search-field"
+              type="text"
+              name="search-field"
+              className="search-field"
+            />
+          </form>
+        </div>
+      );
+  }
 }
 
 export default Search;
