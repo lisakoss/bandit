16a22fbc6dcca1109ed1f33acd549f8f4be3aaaf
Lisa Koss <lisakoss@uw.edu>
2016-12-05 21:26:02 -0800

    Show editing/delete controls to appropriate user

--- src/Listing.js
+++ src/Listing.js
@@ -25,6 +25,12 @@ constructor(props){
 			var listingContent = snapshot.val();
 			this.setState({title: listingContent.title, summary: listingContent.summary, location: listingContent.location, instrument: listingContent.instrument, job: listingContent.job, image: listingContent.image, tags: listingContent.tags, post: listingContent.text, type: listingContent.type, listingUser: listingContent.userId})
 
+			// only show edit controls on a user's own posts
+			if(snapshot.child('userId').val() === firebase.auth().currentUser.uid) {
+				this.setState({showControls: 'show'});
+			}
+
+			// update user's display name and avatar image
 			var usersRef = firebase.database().ref('users/' + listingContent.userId);
 			usersRef.on('value', (snapshot) => {
 				this.setState({displayName: snapshot.child('displayName').val()});
@@ -91,7 +97,27 @@ constructor(props){
     hashHistory.push(path);
 	}
 
+	/* Shows edit and delete buttons for each individual post. */
+	showControls() {
+		var thisComponent = this;
+		var userRef = firebase.database().ref('messages/' + this.props.params.listingName);
+		userRef.once("value")
+			.then(function(snapshot) {
+				var childKey = snapshot.child('userId').val();
+				console.log(childKey);
+				if(childKey === firebase.auth().currentUser.uid) { // only shows controls on a user's own posts'
+					thisComponent.setState({showControls: 'show'});
+				}
+			});
+	}
+
+	/* Hides edit and delete buttons for each individual post. */
+	hideControls() {
+		this.setState({showControls: 'hidden'});
+	}
+
   render() {
+		console.log(this.state);
 		var listingImage = '';
     var editContent = null;
     var lastEdited = '';
@@ -119,6 +145,20 @@ constructor(props){
     return (
       <div className="content-container">
 			  <div className="listing-img" style={{background: 'url(' + listingImage + ') center / cover'}}>
+									<div className={this.state.showControls}>
+						<span className="edit {edited}" onClick={this.handleOpenDialog}><i className="fa fa-trash-o" aria-hidden="true"></i></span>
+						<Dialog open={this.state.openDialog}>
+							<DialogTitle>Delete Post?</DialogTitle>
+							<DialogContent>
+								<p>Are you sure you want to permanently delete this post?</p>
+							</DialogContent>
+							<DialogActions>
+								<Button type='button' onClick={() => this.deleteMessage()}>Delete</Button>
+								<Button type='button' onClick={this.handleCloseDialog}>Cancel</Button>
+							</DialogActions>
+						</Dialog>
+						<span className="edit {edited}" onClick={() => this.editMessage()}>editing controls: <i className="fa fa-pencil" aria-hidden="true"></i></span>
+					</div>
 					<h1 className="listing-title">{this.state.title}</h1>
 					<span className="listing-tags">{this.state.tags}</span>
 				</div>
@@ -137,21 +177,6 @@ constructor(props){
 						<span><strong>Instruments/Skills</strong>: {this.state.instrument}</span>
 						<div className="listing-desc">{editContent}</div>
 					</span>
-
-					<div className="show">
-						<span className="edit {edited}" onClick={this.handleOpenDialog}><i className="fa fa-trash-o" aria-hidden="true"></i></span>
-						<Dialog open={this.state.openDialog}>
-							<DialogTitle>Delete Post?</DialogTitle>
-							<DialogContent>
-								<p>Are you sure you want to permanently delete this post?</p>
-							</DialogContent>
-							<DialogActions>
-								<Button type='button' onClick={() => this.deleteMessage()}>Delete</Button>
-								<Button type='button' onClick={this.handleCloseDialog}>Cancel</Button>
-							</DialogActions>
-						</Dialog>
-						<span className="edit {edited}" onClick={() => this.editMessage()}><i className="fa fa-pencil" aria-hidden="true"></i></span>
-					</div>
 				</div>
       </div>
     );
