8272cb9bcd45c32c6f212c82a73a573bae2c2eaa
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2016-12-05 20:21:46 -0800

    Iterate through posts

--- src/Search.js
+++ src/Search.js
@@ -6,6 +6,44 @@ class Search extends React.Component {
   constructor(props){
     super(props);
     this.state = {};
+
+    // Function binding
+    this.hasSearchTerm = this.hasSearchTerm.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  componentDidMount() {
+    // Create new database reference 'postsRef' and 'usersRef'
+    var postsRef = firebase.database().ref('posts');
+    var usersRef = firebase.database().ref('users');
+
+    // Obtain database objects
+    postsRef.once('value').then(snapshot => {
+      this.setState({posts: snapshot.val()});
+    });
+    usersRef.once('value').then(snapshot => {
+      this.setState({users: snapshot.val()});
+    });
+  }
+
+  // Returns a boolean; Does post have searchTerm?
+  hasSearchTerm(post, searchTerm) {
+    return true;
+  }
+
+  // Update state for specific field
+  handleSearch(event) {
+    var field = event.target.name; // search-field
+    var searchTerm = event.target.value; // whatever is typed
+
+    var posts = this.state.posts;
+
+    // Iterate through posts
+    Object.keys(posts).forEach(function(key, index) {
+      var post = posts[key];
+      // Does this post have a value that contains the search term?
+
+    });
   }
 
   render() {
