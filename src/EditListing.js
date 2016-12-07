68e71455b9d6ff9c015e0bea3f3e6068e116e5e9
Lisa Koss <lisakoss@uw.edu>
2016-12-06 23:41:35 -0800

    Add edit listing error checks

--- src/EditListing.js
+++ src/EditListing.js
@@ -8,11 +8,17 @@ class EditListing extends React.Component {
     super(props);
     this.state = {
         "post":'',
+        "postTyped": false,
         "title": '',
+        "titleTyped": false,
         "summary": '',
+        "summaryTyped": false,
         "location": '',
+        "locationTyped": false,
         "instrument": '',
+        "instrumentTyped": false,
         "job": '',
+        "jobTyped": false,
         "image": '',
         "tags": '',
         "type": 'wanted'
@@ -53,34 +59,34 @@ class EditListing extends React.Component {
     });
 	}
 
-	/* Tracks changes in message input field. */
+/* Tracks changes in message input field. */
   updatePost(event) {
-    this.setState({post: event.target.value});
+    this.setState({post: event.target.value, postTyped: true});
   }
 
   /* Tracks changes in message input field. */
   updateTitle(event) {
-    this.setState({title: event.target.value});
+    this.setState({title: event.target.value, titleTyped: true});
   }
 
   /* Tracks changes in message input field. */
   updateSummary(event) {
-    this.setState({summary: event.target.value});
+    this.setState({summary: event.target.value, summaryTyped: true});
   }
 
   /* Tracks changes in message input field. */
   updateLocation(event) {
-    this.setState({location: event.target.value});
+    this.setState({location: event.target.value, locationTyped: true});
   }
 
   /* Tracks changes in message input field. */
   updateInstrument(event) {
-    this.setState({instrument: event.target.value});
+    this.setState({instrument: event.target.value, instrumentTyped: true});
   }
 
   /* Tracks changes in message input field. */
   updateJob(event) {
-    this.setState({job: event.target.value});
+    this.setState({job: event.target.value, jobTyped: true});
   }
 
   /* Tracks changes in message input field. */
@@ -107,9 +113,11 @@ class EditListing extends React.Component {
 		var newJob = this.state.job;
 		var newImage = this.state.image;
 		var newTags = this.state.tags;
-		
-    if(this.state.post.length !== 0) {
-      event.preventDefault(); // don't submit like usual
+
+		event.preventDefault(); // don't submit like usual
+    if(this.state.post.length !== 0 && this.state.title.length !== 0 && this.state.summary.length !== 0 
+      && this.state.summary.length < 100 && this.state.location.length !== 0 && this.state.instrument.length !== 0
+      && this.state.job.length !== 0) {
 
       /* Add a new channel message to the database. */
       var messageRef = firebase.database().ref('posts/' + this.props.params.listingName);
@@ -122,10 +130,27 @@ class EditListing extends React.Component {
 			messageRef.child('job').set(newJob);
 			messageRef.child('image').set(newImage);
 			messageRef.child('tags').set(newTags);
-      this.setState({edit:false});
 
 			const path = '/posts/' + this.props.params.listingName; // redirects user back to listing after editing
 			hashHistory.push(path);
+    } else {
+      if(this.state.post.length === 0) {
+        this.setState({postTyped: true});
+      } if(this.state.title.length === 0) {
+        this.setState({titleTyped: true});
+      } 
+      if(this.state.summary.length === 0) {
+        this.setState({summaryTyped: true});
+      }
+      if(this.state.location.length === 0) {
+        this.setState({locationTyped: true});
+      }
+      if(this.state.instrument.length === 0) {
+        this.setState({instrumentTyped: true});
+      }
+      if(this.state.job.length === 0) {
+        this.setState({jobTyped: true});
+      }
     }
   }
 	
@@ -153,6 +178,9 @@ class EditListing extends React.Component {
 							value={this.state.title}
 							className="msg-input"
 						/>
+						{this.state.title.length === 0 && this.state.titleTyped === true &&
+            	<p className="help-block">Title cannot be left blank.</p>
+          	}
 						<Textfield
 							onChange={(e) => this.updateSummary(e)}
 							label="short summary"
@@ -161,8 +189,11 @@ class EditListing extends React.Component {
 							className="msg-input"
 						/>
 						{this.state.summary.length > 100 &&
-							<p className="help-block">100 character limit!</p>
-						}
+            	<p className="help-block">100 character limit.</p>
+          	}
+						{this.state.summary.length === 0 && this.state.summaryTyped === true &&
+            	<p className="help-block">Summary cannot be left blank.</p>
+          	}
 						<Textfield
 							onChange={(e) => this.updateLocation(e)}
 							label="location (city, state, &amp; zip code)"
@@ -187,6 +218,15 @@ class EditListing extends React.Component {
 							className="msg-input-type"
 							style={{width: '33%'}}
 						/>
+						{this.state.location.length === 0 && this.state.locationTyped === true &&
+            	<p className="help-block">Location cannot be left blank.</p>
+						}
+						{this.state.instrument.length === 0 && this.state.instrumentTyped === true &&
+							<p className="help-block">Instrument(s) and/or skills cannot be left blank.</p>
+						}
+						{this.state.job.length === 0 && this.state.jobTyped === true &&
+							<p className="help-block">Job cannot be left blank.</p>
+						}
 						<div className="post-img" style={{background: 'url(' + listingImage + ') center / cover'}} />
 						<Textfield
 							onChange={(e) => this.updateImage(e)}
@@ -213,6 +253,9 @@ class EditListing extends React.Component {
 							rows={6}
 							className="msg-input"
 						/>
+						{this.state.post.length === 0 && this.state.postTyped === true &&
+            	<p className="help-block">Listing description cannot be left blank.</p>
+          	}
 
 						<Button ripple className="create-button" onClick={(e) => this.postMessage(e)}>Edit Listing</Button>
 
