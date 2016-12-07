f18e9c62a06cfc64f0811fafe7e9fadff887d8c2
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2016-12-07 12:25:12 -0800

    Fix merge conflict

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import firebase from 'firebase';
 import { Tooltip, Tabs, Tab } from 'react-mdl';
+import RecentListings from './RecentListings.js';
+import { hashHistory } from 'react-router';
 
 class Profile extends React.Component {
   constructor(props){
@@ -8,31 +10,39 @@ class Profile extends React.Component {
 		this.state = {activeTab: 0};
 	}
 
-
 	//Lifecycle callback executed when the component appears on the screen.
-  //It is cleaner to use this than the constructor for fetching data
-  componentDidMount() {
-	this.unregister = firebase.auth().onAuthStateChanged(user => {
-		if(user) {
-			this.setState({userId: user.uid});
-			this.setState({displayName: firebase.auth().currentUser.displayName});
-			//this.setState({avatar: firebase.auth().currentUser.photoURL});
-			var profileRef = firebase.database().ref('users/' + this.props.params.profileID);
-			profileRef.once("value")
-				.then(snapshot => {
-					//this.setState({displayName: snapshot.child("displayName").val()});
-					this.setState({avatar: snapshot.child("avatar").val()});
-					this.setState({coverPhoto: snapshot.child("coverPhoto").val()});
-					this.setState({jobTitle: snapshot.child("jobTitle").val()});
-					this.setState({instruments: snapshot.child("instruments").val()});
-					this.setState({genre: snapshot.child("genre").val()});
-					this.setState({location: snapshot.child("location").val()});
-					this.setState({about: snapshot.child("about").val()});
-					this.setState({experience: snapshot.child("experience").val()});
-				});
-		}
-      else{
-        this.setState({userId: null}); //null out the saved state
+	//Grabs information to display on a user's profile'
+	componentDidMount() {
+		this.unregister = firebase.auth().onAuthStateChanged(user => {
+			if(user) {
+				this.setState({userId: user.uid}); //grabs user id
+				var profileRef = firebase.database().ref('users/' + this.props.params.profileID);
+				profileRef.once("value")
+					.then(snapshot => {
+						this.setState({displayName: snapshot.child("displayName").val()});
+						this.setState({avatar: snapshot.child("avatar").val()});
+						this.setState({coverPhoto: snapshot.child("coverPhoto").val()});
+						this.setState({jobTitle: snapshot.child("jobTitle").val()});
+						this.setState({instruments: snapshot.child("instruments").val()});
+						this.setState({genre: snapshot.child("genre").val()});
+						this.setState({location: snapshot.child("location").val()});
+						this.setState({about: snapshot.child("about").val()});
+						this.setState({experience: snapshot.child("experience").val()});
+					});
+
+				if(this.state.instruments === "") {
+					this.setState({instrumentShow: 'hidden'}); //hides instruments if not filled out
+				}
+				if(this.state.genre === "") {
+					this.setState({genreShow: 'hidden'}); //hides genre if not filled out
+				}
+				if(this.state.location === "") {
+					this.setState({locationShow: 'hidden'}); //hides location if not filled out
+				}
+			} else { //redirects user if logged out and sets state to null
+				const path = '/login';
+				hashHistory.push(path);
+				this.setState({userId: null}); //null out the saved state
 				this.setState({displayName: null}); //null out the saved state
 				this.setState({avatar: null}); //null out the saved state
 				this.setState({jobTitle: null}); //null out the saved state
@@ -42,19 +52,20 @@ class Profile extends React.Component {
 				this.setState({location: null}); //null out the saved state
 				this.setState({about: null}); //null out the saved state
 				this.setState({experience: null}); //null out the saved state
-      }
-    })
-  }
+			}
+		})
+	}
 
-  //when component will be removed
-  componentWillUnmount() {
-  	//unregister listeners
-  	firebase.database().ref('users/' + this.props.params.profileID).off();
+	//when component will be removed
+	componentWillUnmount() {
+		//unregister listeners
+  		firebase.database().ref('users/' + this.props.params.profileID).off();
 		if(this.unregister){ //if have a function to unregister with
-      this.unregister(); //call that function!
-    }
-  }
+     		this.unregister(); //call that function!
+   		}
+  	}
 
+	//Grabs the next user's information'
 	componentWillReceiveProps(nextProps) {
 		var profileRef = firebase.database().ref('users/' + nextProps.params.profileID);
 		profileRef.once("value")
@@ -69,36 +80,58 @@ class Profile extends React.Component {
 				this.setState({about: snapshot.child("about").val()});
 				this.setState({experience: snapshot.child("experience").val()});
 			});
-  }
+	}
 
 	render() {
-		console.log(this.state);
 		var divStyle = {
  			backgroundImage: 'url(' + this.state.coverPhoto || ' )'
 		};
 		var edit = null;
 		var tabContent = null;
+		var htmlTabContent = null;
+		var locationHide = 'hidden';
+		var genreHide = 'hidden';
+		var instrumentsHide = 'hidden';
 
+		//if you are on your own profile, you have access to the profile edit tool via your profile
 		if(this.props.params.profileID === this.state.userId) {
 			edit = "icon";
 		} else {
 			edit = "icon hidden"
 		}
 
+		//determines tab content for about, experience, and recent listings
 		if(this.state.activeTab === 0) {
-			tabContent = this.state.about;
+			htmlTabContent = this.state.about || "No information to show.";
+			tabContent = (<div className="tab-content" dangerouslySetInnerHTML={{__html:htmlTabContent}}></div>);
 		} else if(this.state.activeTab === 1) {
-			tabContent = this.state.experience;
+			htmlTabContent = this.state.experience || 'No experience to show.';
+			tabContent = (<div className="tab-content" dangerouslySetInnerHTML={{__html:htmlTabContent}}></div>);
 		} else {
-			tabContent = "No recent listings to show.";
+			tabContent = (<div className="tab-content"><RecentListings profileID={this.props.params.profileID} /></div>);
+		}
+
+		//show location if filled out
+		if(this.state.location !== null && this.state.location !== "") {
+			locationHide = '';
+		}
+
+		//show instruments if filled out
+		if(this.state.instruments !== null && this.state.instruments !== "") {
+			instrumentsHide = '';
+		}
+
+		//show genre if filled out
+		if(this.state.genre !== null && this.state.genre !== "") {
+			genreHide = '';
 		}
 
 		return (
-			<div id="profile">
-				<div className="profile-top" style={divStyle}>
+			<div role="article">
+				<div role="region" className="profile-top" style={divStyle}>
 					<p className={edit}><a href="/#/profileedit"><i className="fa fa-pencil edit-profile" aria-hidden="true"> <span className="edit-profile-text">edit</span></i></a></p>
 				</div>
-				<div className="profile-user">
+				<div role="region" className="profile-user">
 					<div className="content-container">
 						<div className="profile-image">
 							<img src={this.state.avatar || './img/blank-user.jpg'} alt="avatar" />
@@ -107,21 +140,21 @@ class Profile extends React.Component {
 						<p className="job-title">{this.state.jobTitle}</p>
 
 						<div className="profile-stats">
-							<span><Tooltip label="Instruments and Skills" position="top"><i className="fa fa-music display-icon" aria-hidden="true"></i></Tooltip><p> {this.state.instruments}</p></span>
-							<span><Tooltip label="Genre" position="top"><i className="fa fa-headphones display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> {this.state.genre}</p></span>
-							<span><Tooltip label="Location" position="top"><i className="fa fa-map-marker display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> {this.state.location}</p></span>
-							<span><Tooltip label="Contact" position="top"><i className="fa fa-comments display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> <a href="/#/inbox">Contact {this.state.displayName}</a></p></span>
+							<span className={instrumentsHide}><Tooltip label="Instruments and Skills" position="top"><i className="fa fa-music display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> {this.state.instruments}</p></span>
+							<span className={genreHide}><Tooltip label="Genre" position="top"><i className="fa fa-headphones display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> {this.state.genre}</p></span>
+							<span className={locationHide}><Tooltip label="Location" position="top"><i className="fa fa-map-marker display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> {this.state.location}</p></span>
+							<span><Tooltip label="Contact" position="top"><i className="fa fa-comments display-icon" aria-hidden="true"></i></Tooltip><p className="quick-info"> <a href="mailto:my.pencil.info@gmail.com">Contact {this.state.displayName}</a></p></span>
 						</div>
 					</div>
-					<div className="profile-tabs">
+					<div role="region" className="profile-tabs">
 						<div className="content-container">
 							<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
 								<Tab>About</Tab>
 								<Tab>Experience</Tab>
 								<Tab>Recent Listings</Tab>
-              </Tabs>
+             				</Tabs>
 							<section>
-								<div className="tab-content">{tabContent}</div>
+								{tabContent}
 							</section>
 						</div>
 					</div>
@@ -130,5 +163,6 @@ class Profile extends React.Component {
 		);
 	}
 }
+
 
 export default Profile;
--- src/SignIn.js
+++ src/SignIn.js
@@ -12,11 +12,10 @@ import { Textfield, Button } from 'react-mdl';
 class SignInForm extends React.Component {
   constructor(props){
     super(props);
-
     this.state = {
       'email': undefined,
       'password': undefined
-    }; 
+    };
 
     //function binding
     this.handleChange = this.handleChange.bind(this);
@@ -42,13 +41,13 @@ class SignInForm extends React.Component {
 
   /**
    * A helper function to validate a value based on a hash of validations
-   * second parameter has format e.g., 
+   * second parameter has format e.g.,
    * {required: true, minLength: 5, email: true}
    * (for required field, with min length of 5, and valid email)
    */
   validate(value, validations) {
     var errors = {isValid: true, style:''};
-    
+
     if(value !== undefined) { //check validations
       if(validations.required && value === '') {
         errors.required = true;
@@ -75,7 +74,7 @@ class SignInForm extends React.Component {
     //display details
     if(!errors.isValid){ //if found errors
       errors.style = 'has-error';
-      
+
     }
     else if(value !== undefined){ //valid and has input
       //errors.style = 'has-success' //show success coloring
@@ -95,7 +94,7 @@ class SignInForm extends React.Component {
     var signInEnabled = (emailErrors.isValid && passwordErrors.isValid);
 
     return (
-      <div>
+      <div role="article">
         <h1>sign in</h1>
 
         <form role="form" className="sign-up-form">
@@ -137,14 +136,14 @@ class ValidatedInput extends React.Component {
         <ValidationErrors errors={this.props.errors} />
       </div>
     );
-  }  
+  }
 }
 
 //a component to represent and display validation errors
 class ValidationErrors extends React.Component {
   render() {
     return (
-      <div>
+      <div role="region">
         {this.props.errors.required &&
           <span className="help-block">Required! </span>
         }
@@ -152,8 +151,8 @@ class ValidationErrors extends React.Component {
           <span className="help-block">Not an email address!</span>
         }
         {this.props.errors.minLength &&
-          <span className="help-block">Must be at least {this.props.errors.minLength} character(s).</span>        
-        } 
+          <span className="help-block">Must be at least {this.props.errors.minLength} character(s).</span>
+        }
       </div>
     );
   }
